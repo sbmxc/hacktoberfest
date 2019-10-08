8da771d3cc6fd57e99a5305e3b926d16b0185059
sljtheultima <sljtheultima@users.noreply.github.com>
2019-10-08 20:37:17 +0800

    Create Hello_world_sljtheultima.m

---
+++ Hello_world_sljtheultima.m
@@ -0,0 +1,11 @@
+// First program example
+ 
+#import <Foundation/Foundation.h>
+ 
+int main (int argc, const char * argv[])
+{
+        NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+        NSLog (@"Hello, World!");
+        [pool drain];
+        return 0;
+}
