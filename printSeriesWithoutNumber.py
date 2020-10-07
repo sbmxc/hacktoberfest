e086d124a9436229063dbf4f7e73c631ba41ab14
imakshat47 <imakshat@github.com>
2020-10-08 00:25:19 +0530

    Added new Programs in Python

---
+++ printSeriesWithoutNumber.py
@@ -0,0 +1,5 @@
+# A program to print out all numbers from 70 to 80 without using a single number in his program and also restricts the character limit to 100.
+
+# Write your code here
+for i in 'FGHIJKLMNOP':
+    print(ord(i))
