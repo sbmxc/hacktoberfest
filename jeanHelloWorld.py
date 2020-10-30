3bc1d5a6517a69adae25a4ad2ade02b827d5ba50
Jean-mrs <jeanmoraes1994@hotmail.com>
2020-10-30 00:25:54 -0300

    meu primeiro pull request!

---
+++ jeanHelloWorld.py
@@ -0,0 +1,11 @@
+# Name: Jean Hello World
+# Date: 29/10/20
+# Description: printing a "Hello World!" script.
+
+
+# main() defined here
+def main():
+    print("Esse eh meu primeiro Hello, World!!!!")
+
+# main() is called here
+main()
