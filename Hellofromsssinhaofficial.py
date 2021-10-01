e226bcd8aa63875e17e647fbee8944e0367657b9
Shiv Shikhar Sinha <shiv.shikhar.3@gmail.com>
2021-10-01 19:36:24 +0530

    Added Hello world program in a loop for greeting

---
+++ Hellofromsssinhaofficial.py
@@ -0,0 +1,7 @@
+print("Hello World")
+print("This is my first pull request in Hactoberfet 2021")
+print("Feeling very excited for this years event.")
+print("Enter number of time you want to print hello: ")
+n=int(input("Enter an Integer: "))
+for i in range (n):
+    print("Hello!!!")
