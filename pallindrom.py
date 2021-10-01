0a6251eb4c93b413a8d43a97afedaacc3db95791
Ishan Goyal <66260653+ishangoyal8055@users.noreply.github.com>
2021-10-01 11:25:26 +0530

    Update pallindrom.py

--- pallindrom.py
+++ pallindrom.py
@@ -1,11 +1,6 @@
-n=int(input("Enter number:"))
-temp=n
-rev=0
-while(n>0):
-    dig=n%10
-    rev=rev*10+dig
-    n=n//10
-if(temp==rev):
+number=int(input("Enter number:"))
+rev=int(str(n)[::-1])
+if(number==rev):
     print("The number is a palindrome!")
 else:
     print("The number isn't a palindrome!")
