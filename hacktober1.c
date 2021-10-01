19a05a9cd8d93ca20e3e49c7be43da2498ce6d71
arrshsh <84338598+arrshsh@users.noreply.github.com>
2021-10-01 11:15:06 +0530

    Hello World in C
    This is the program to print Hello World

---
+++ hacktober1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+int main()
+{
+     printf ("Hello World!");
+     return 0;
+}
