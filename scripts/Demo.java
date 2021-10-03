914e27c8dce101c2eaefcfa7be5769a42c30821e
KaveeshaGallage <48401939+KaveeshaGallage@users.noreply.github.com>
2021-10-03 00:40:46 -0700

    simple java calculator application

---
+++ scripts/Demo.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+class Demo{
+	public static void main(String args[]){
+		JFrame f1=new JFrame();
+		f1.setSize(300,300);
+		f1.setTitle("Calculator");
+		f1.setDefaultCloseOperation(3);	
+		f1.setLocationRelativeTo(null);
+		f1.setVisible(true);
+	}
+}
