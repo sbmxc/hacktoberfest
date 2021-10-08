b3bf716be45af3dba760ceccd6bbc5966b91523f
Sayan Chattopadhyay <sayan6823.ch@gmail.com>
2021-10-08 09:20:44 +0530

    added a program to calculate sum of digits of a 5digit number

---
+++ 5digsum.c
@@ -0,0 +1,33 @@
+/*   This is just a sum of digitd calculating program for a 5 digit number  */
+
+
+#include<stdio.h>
+#include<conio.h>
+
+
+void main()
+{
+    int n,sum; sum=0;
+    printf("\n Enter the number");
+    scanf("%d",&n);
+    int i;
+    while(n>0)
+    {
+        i=n%10;
+        sum=sum+i;
+        n=n/10;
+    }
+    
+
+
+
+
+    printf("\n The sum of the digits is %d",sum);
+
+
+
+
+
+
+    getch();
+}
