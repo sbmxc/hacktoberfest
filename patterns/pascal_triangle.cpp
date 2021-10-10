ea6b4c0d539416c23e0250cb6a6eb8790e1da1f2
Ruhika <65492460+Ruhika1417@users.noreply.github.com>
2021-10-10 12:49:15 +0530

    pascal's triangle

--- patterns/pascal_triangle.cpp
+++ patterns/pascal_triangle.cpp
@@ -30,8 +30,8 @@ cin>>n;
 
 for (int i=0; i<n;i++){
     for (int j=0;j<=i;j++){
-    cout<<fact(i)/(fact(j)*fact(i-j))<< " ";    //for int i=0 cout will be printed 1 time
-    }                                           //int i=1; cout 2 times
+    cout<<fact(i)/(fact(j)*fact(i-j))<< " ";    
+    }                                           
     cout<<endl;
 }
 
