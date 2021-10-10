b420b497c19620c339369571617e9c4a273a07df
Ruhika <65492460+Ruhika1417@users.noreply.github.com>
2021-10-10 12:52:14 +0530

    square star pattern

---
+++ patterns/pattern1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    for (int i=0; i<5; i++){
+       for (int j=0; j<4;j++){
+           cout <<" * ";
+         //break;     
+       }
+       
+           cout<<endl;        //use this instead
+    }  
+    
+    }
