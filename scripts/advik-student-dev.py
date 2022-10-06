0ed21d59a3d3177bee2375ff646a4bc9a73cd7df
Advik Sharma <70201060+advik-student-dev@users.noreply.github.com>
2022-10-06 16:40:27 -0700

    advik-student-dev commits

---
+++ scripts/advik-student-dev.py
@@ -0,0 +1,12 @@
+if __name__ == "__main__":
+    print("Program Starts from here!")
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+p1 = Person("Max Verstappen")
+p2 = Person("Yo mama")
+print("\n", p1.name, "\n")
+print(p2.name + " so FAT")
+
