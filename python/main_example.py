ea51faa09aeb78fb5735edd3680fca2f6108214f
Hathaipach <hathaipach@hotmail.com>
2023-10-09 11:07:39 +0700

    Create main_example.py
    Create main_example.py

---
+++ python/main_example.py
@@ -0,0 +1,14 @@
+def greet(name):
+    return f"Hello, {name}!"
+
+def add(a, b):
+    return a + b
+
+name = "Alice"
+result = greet(name)
+print(result)  # Output: Hello, Alice!
+
+num1 = 5
+num2 = 3
+sum_result = add(num1, num2)
+print(sum_result)  # Output: 8
