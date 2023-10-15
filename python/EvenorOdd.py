24f8f0cec9022c5bd4fc26f84a8c6b912c244803
ohohoh456 <97571395+ohohoh456@users.noreply.github.com>
2023-10-15 14:28:11 -0700

    Create EvenorOdd.py
    i created a program that tells you if a number you give is even or odd

---
+++ python/EvenorOdd.py
@@ -0,0 +1,5 @@
+number=int(input("give me a number, and ill tell you if its odd."))
+if number % 2 == 1:
+    print(str(number)+ " is an odd number")
+else:
+    print(str(number) + " is an even number")
