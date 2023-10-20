8842f5615b2c71bb7690ee92ff9119753e6d46b3
Anamuddin Ahmad <119073478+AnamuddinAhmad@users.noreply.github.com>
2023-10-20 22:01:34 +0530

    Create Linear_Search.java

---
+++ Linear_Search.java
@@ -0,0 +1,20 @@
+public class Linear_Search {
+
+public static void main(String[] args){
+Scanner sc= new Scanner(System.in);
+System.out.print("Enter : ");
+String find=sc.nextLine();
+int [] array={10,23,20,1,65,87};
+System.out.println(Linear_Search(array));
+sc.close();
+}
+// Linear Search on String
+static int Linear_Search(int [] array) {
+// Find min Value
+for (int i = 0; i <array.length; i++) {
+if(min < array[i]){
+min=array[i];
+}
+}
+}
+}
